5bf4872fc20c17b4739479ae898c68d74b656732
izhakmo <izhakmo@post.bgu.ac.il>
2020-04-07 16:48:10 +0300

    branch typing

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -9,8 +9,8 @@ export const mapMat = <T> (pred : ((x:T)=> T ),elements :T[][]) : T[][] =>
     elements.map(x => x.map(y => pred(y)));
 
 /* Question 3 */
-export const composeMany =<T> (array :Array<((x:T) =>T)>) =>
-array.reduce((acc,cur)=> R.compose(acc,cur),);
+export const composeMany =<T> (array :Array<((x:T) =>T)>) : ((x:T)=>T) =>
+    array.reduce((acc,cur)=> R.compose(acc,cur),);
 
 /* Question 4 */
 interface Languages {
@@ -37,19 +37,20 @@ interface Pokemon {
 }
 
 export const maxSpeed = (pokedex : Pokemon[]) : Pokemon[] =>{    //finding maxSpeed
-    let fastAndFurious=pokedex.reduce((maxSpeed:Number,pikachu :Pokemon) =>
+    let fastAndFurious : Number = pokedex.reduce((maxSpeed:Number,pikachu :Pokemon) =>
     (pikachu.base.Speed>maxSpeed)? maxSpeed=pikachu.base.Speed :maxSpeed,0 );
 
     return pokedex.filter(x =>x.base.Speed==fastAndFurious);   //return array of fastest
 }
 
-export const grassTypes = (pokedex: Pokemon[])=>{
-    let names=pokedex.filter(x=> x.type.includes("Grass"));
+export const grassTypes = (pokedex: Pokemon[]) : string[] =>{
+    let names : Pokemon[] =pokedex.filter(x=> x.type.includes("Grass"));
     return names.map(x => (x.name.english)).sort();
 }
 
 export const uniqueTypes = (pokedex : Pokemon[]) : string[] =>{
     let types :string[][] = pokedex.map(x => x.type);
-    let flatTypes :string[]= types.reduce((acc,cur)=> acc.concat(cur),[]);
-    return flatTypes.reduce((acc : string[],cur )=> acc.includes(cur) ?  acc :acc.concat(cur) ,[]).sort();
+    let flatTypes :string[] = types.reduce((acc,cur)=> acc.concat(cur),[]);
+    return flatTypes.reduce((acc : string[],cur )=> acc.includes(cur) ? 
+    acc :acc.concat(cur) ,[]).sort();
 }
